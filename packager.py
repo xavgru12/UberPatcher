862950df7a20772c6299fe4b4189b5a7a8a8d4d3
xavgru12 <xaver.max.gruber@gmail.com>
2023-08-13 13:33:16 +0200

    working packager, but messed up architecture

--- packager.py
+++ packager.py
@@ -3,26 +3,43 @@ import os
 import shutil
 import sys
 sys.path.append(os.path.join(os.getcwd(),"./Patcher"))
-#sys.path.append(r"C:\Users\Xaver\Documents\Uber\Skripte\UberPatcher")
 import UberPatcher
-#result = subprocess.run(["python -m nuitka --mingw64 --onefile UberPatcher.py"], shell=True, capture_output=True, text=True)
-
-#print(result.stdout)
-
-#result = subprocess.run(["python", "-m", "nuitka", "--mingw64", "UberPatcher.py" ], capture_output = True, text = True, check=True)
-
-#print(result.stdout)
 
 
-#import subprocess
-def runNuitka():
-    current_dir =os.getcwd()
-    print(current_dir)
-    os.chdir("./Patcher/exe")
+def generateExe(patcher, installer):
+
+    patcher_dir="./Patcher/exe"
+    installer_dir="./Installer/exe"
+
+    if patcher is True:
+        runPatch(patcher_dir)
+
+    if installer is True: 
+        runPatch(installer_dir)    
+        
+def runPatch(patcher_dir):
+    current_dir = os.getcwd()
+    os.chdir(patcher_dir)
+    runNuitka(patcher_dir)
+    os.chdir(current_dir)
+
+def runNuitka(path):
+
+    patcher_name:str
+    if "Installer" not in path and "Patcher" not in path:
+        pass
+        #throw Exception
+    if "Installer" in path:
+        patcher_name = "UberInstaller.py"
+    if "Patcher" in path:
+        patcher_name= "UberPatcher.py"
+    
+    patcher_filepath= "../"+patcher_name
+
 
     try:
 
-        output = subprocess.check_output([r"C:\Users\Xaver\AppData\Local\Programs\Python\Python310\python.exe", r"-m", r"nuitka", r"--mingw64", "../UberPatcher.py" ], text=True)
+        output = subprocess.check_output([r"C:\Users\Xaver\AppData\Local\Programs\Python\Python310\python.exe", r"-m", r"nuitka", r"--mingw64", patcher_filepath ], text=True)
 
         print(output)
 
@@ -30,15 +47,9 @@ def runNuitka():
 
         print(f"Command failed with return code {e.returncode}")
 
-    os.chdir(current_dir)
-    print("current dir: "+ os.getcwd())
-    os.mkdir("UberPatcher")
 
-def copyNeededFiles(current_dir=os.getcwd()):
+def copyFiles(destination_dir, filePackageDir, exePath):
 
-    destination_dir = os.path.join(current_dir, "UberPatcher")
-    filePackageDir = os.path.join(current_dir, "Patcher/packageInfo")
-    exePath = os.path.join(current_dir, "Patcher", "exe", "UberPatcher.exe")
 
     UberPatcher.replaceFiles(filePackageDir, destination_dir)
 
@@ -46,14 +57,46 @@ def copyNeededFiles(current_dir=os.getcwd()):
     
     return destination_dir
 
+def copyFilesForZip(patcher, installer):
+
+    current_dir = os.getcwd()
+
+    patcher_destination_dir:str
+    installer_destination_dir:str
+
+    if patcher is True:
+        folder_name = "UberPatcher"
+        os.mkdir(folder_name)
+        patcher_destination_dir = os.path.join(current_dir, folder_name)
+        filePackageDir = os.path.join(current_dir, "Patcher/packageInfo")
+        exePath = os.path.join(current_dir, "Patcher", "exe", "UberPatcher.exe")
+        copyFiles(patcher_destination_dir, filePackageDir, exePath)
+    if installer is True:
+        folder_name = "UberInstaller"
+        os.mkdir(folder_name)
+        installer_destination_dir = os.path.join(current_dir, folder_name)
+        filePackageDir = os.path.join(current_dir, "Installer/packageInfo")
+        exePath = os.path.join(current_dir, "Installer", "exe", "UberInstaller.exe")
+        copyFiles(installer_destination_dir, filePackageDir, exePath)
+
+    return patcher_destination_dir, installer_destination_dir
 
 def createZip(path):
 
-    shutil.make_archive(path, 'zip', path)
+    zip_file_name_with_path = path
+    folder_path = path
+    shutil.make_archive(zip_file_name_with_path, 'zip', folder_path)
 
 if __name__ == "__main__":
-    runNuitka()
-    tempFolderForZip = copyNeededFiles()
-    createZip(tempFolderForZip)
-    shutil.rmtree(tempFolderForZip)
+    generateExe(True, True)
+    tempFolderForZipPatcher, tempFolderForZipInstaller = copyFilesForZip(True, True)
+    if tempFolderForZipPatcher != "":
+        createZip(tempFolderForZipPatcher)
+        shutil.rmtree(tempFolderForZipPatcher)
+
+    if tempFolderForZipInstaller != "":
+        createZip(tempFolderForZipInstaller) 
+        shutil.rmtree(tempFolderForZipInstaller)   
+
+
 
